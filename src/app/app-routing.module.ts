bf0354b690843b4f15a31d26bb534bcd78ee77f8
maxwellfundi <fundimaxwell@gmail.com>
2020-11-14 09:48:56 +0300

    editing routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,15 +2,17 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AboutComponent } from './about/about.component';
 import { ContactComponent } from './contact/contact.component';
+import { DevelopersComponent } from './developers/developers.component';
+import { DownloadComponent } from './download/download.component';
 import { HomeComponent } from './home/home.component';
+import { SupportComponent } from './support/support.component';
 
 const routes: Routes = [
   {
    path: 'home',
    component: HomeComponent
   },
-
-  {
+ {
     path: 'contact',
     component: ContactComponent
   }, 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { ContactComponent } from './contact/contact.component';
-import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { AboutComponent } from './about/about.component';
 import { DevelopersComponent } from './developers/developers.component';
 import { SupportComponent } from './support/support.component';
 import { DownloadComponent } from './download/download.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
